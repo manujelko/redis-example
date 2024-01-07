d97da1bc8f375f881b5327cc103f1543f837028e
Manuel Šarić <manujelko@gmail.com>
2024-01-07 12:22:27 +0100

    Add sets example

---
+++ app/sets_example.py
@@ -0,0 +1,26 @@
+import asyncio
+
+import aioredis
+
+
+async def main():
+    # Connect to Redis
+    redis = aioredis.from_url("redis://localhost", encoding="utf-8", decode_responses=True)
+
+    # Add elements to a set
+    await redis.sadd("myset", "a", "b", "c")
+
+    # Check if an element is in the set
+    is_member = await redis.sismember("myset", "a")
+    print(f"'a' is a member of set: {is_member}")
+
+    # Get all memebers of the set
+    members = await redis.smembers("myset")
+    print(f"Set members: {members}")
+
+    # Close the connection
+    await redis.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
